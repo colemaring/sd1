bdfe2b9ed56a3a064fd44165a318f31796559138
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-10-09 21:30:20 -0400

    init

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/aifsd.xyz/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/aifsd.xyz/fullchain.pem"),
+};
+
+// init https server
+const httpsServer = https.createServer(options, app);
+
+// Create an HTTP server to redirect to HTTPS
+const httpServer = http.createServer((req, res) => {
+  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+  res.end();
+});
+
+// Render built react files
+app.use(express.static("dist"));
+
+// for all routes, return index.html
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+httpsServer.listen(443, () => {
+  console.log("Server started on port 443");
+});
+
+// exists only to redirect http to https
+httpServer.listen(80, () => {
+  console.log("HTTP redirect server started on port 80");
+});
