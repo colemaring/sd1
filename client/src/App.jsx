614357d8e8af078d29cf5eb423e8b05117255e8b
Maria Fernanda <fefepanda@gmail.com>
2024-10-16 00:07:54 -0400

    sidebar collapse function added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Maps from "./components/Maps"; // Import the Maps component
 import { Col, Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SideNavbar from "./components/SideNavbar";
 
 function App() {
-  console.log("im cool w not using bootstrap");
+  const [sidebarSize, setSidebarSize] = useState("");
+
+  const getSizeChange = (sizedata) => {
+    setSidebarSize(sizedata);
+  };
+
+  // console.log("im cool w not using bootstrap");
   return (
     <>
       <Container fluid>
         <Row>
-          <Col xl={2} > 
-            <SideNavbar />
+          <Col xl={sidebarSize}>
+            <SideNavbar sendSizeChange={getSizeChange} />
           </Col>
-          <Col xl={10} >
+          <Col xl={10}>
             <Maps />
           </Col>
         </Row>
--- client/src/components/SideNavbar.jsx
+++ client/src/components/SideNavbar.jsx
@@ -1,38 +1,91 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
+import { Nav, Button } from "react-bootstrap";
 import { useState } from "react";
 import "../App.css";
 import { MdOutlineLocationOn, MdCommute } from "react-icons/md";
 import { IoCarOutline } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
 
-
-
+// Will refactor the code later
 // TODO: Link pages
-export default function SideNavbar() {
+export default function SideNavbar({ sendSizeChange }) {
+  const [collapsed, setCollapsed] = useState(false);
   const [active, setActive] = useState("default");
+  const data = 2;
+
+  if (!collapsed) {
+    const data = 1;
+    return (
+      <>
+        <Nav
+          variant="pills"
+          className="flex-column me-auto"
+          defaultActiveKey="Map"
+          onSelect={(selectedKey) => setActive(selectedKey)}
+        >
+          <Nav.Item className="header-ai">
+            AI for Safe Driving
+            <Button
+              className="collapse-button"
+              onClick={() => {
+                sendSizeChange(data);
+                setCollapsed(true);
+              }}
+            >
+              <RxHamburgerMenu size={30} />
+            </Button>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#" eventKey="Map">
+              <MdOutlineLocationOn size={30} /> {"  "}Fleet Map
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="fw-lighter border-bottom">Statistics</Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#" eventKey="Fleet">
+              <MdCommute size={30} /> {"  "}Fleet
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#" eventKey="Driver">
+              <IoCarOutline size={30} /> {"  "} Driver
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </>
+    );
+  }
+
   return (
     <>
       <Nav
         variant="pills"
-        className="flex-column me-auto .pills.active.rounded-circle"
+        className="flex-column me-auto collapsed"
         defaultActiveKey="Map"
         onSelect={(selectedKey) => setActive(selectedKey)}
       >
-        <Nav.Item className="header-ai">AI for Safe Driving</Nav.Item>
+        <Button
+          className="collapsed-button"
+          onClick={() => {
+            sendSizeChange(data);
+            setCollapsed(false);
+          }}
+        >
+          <RxHamburgerMenu size={30} />
+        </Button>
         <Nav.Item>
           <Nav.Link href="#" eventKey="Map">
-          <MdOutlineLocationOn size={30}/> {"  "}Fleet Map
+            <MdOutlineLocationOn size={30} />
           </Nav.Link>
         </Nav.Item>
-        <Nav.Item className="fw-lighter border-bottom">Statistics</Nav.Item>
         <Nav.Item>
           <Nav.Link href="#" eventKey="Fleet">
-          <MdCommute size={30}/> {"  "}Fleet
+            <MdCommute size={30} />
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link href="#" eventKey="Driver">
-          <IoCarOutline size={30}/> {"  "} Driver
+            <IoCarOutline size={30} />
           </Nav.Link>
         </Nav.Item>
       </Nav>
