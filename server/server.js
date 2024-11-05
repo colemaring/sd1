dc00466b9ebd9696e60695614e39d9ead50d1fc8
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-11-05 18:38:17 -0500

    add development flag

--- server/server.js
+++ server/server.js
@@ -4,33 +4,46 @@ const https = require("https");
 const http = require("http");
 const fs = require("fs");
 
-const options = {
-  key: fs.readFileSync("/etc/letsencrypt/live/aifsd.xyz/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/aifsd.xyz/fullchain.pem"),
-};
+const isDev = process.argv.includes("dev");
 
-// init https server
-const httpsServer = https.createServer(options, app);
+let httpsServer, httpServer;
 
-// Create an HTTP server to redirect to HTTPS
-const httpServer = http.createServer((req, res) => {
-  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-  res.end();
-});
+if (!isDev) {
+  const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/aifsd.xyz/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/aifsd.xyz/fullchain.pem"),
+  };
+
+  // init https server
+  const httpsServer = https.createServer(options, app);
+
+  // Create an HTTP server to redirect to HTTPS
+  const httpServer = http.createServer((req, res) => {
+    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+    res.end();
+  });
+
+  httpsServer.listen(443, () => {
+    console.log("Server started on port 443");
+  });
+
+  // exists only to redirect http to https
+  httpServer.listen(80, () => {
+    console.log("HTTP redirect server started on port 80");
+  });
+} else {
+  // dev server
+  httpServer = http.createServer(app);
+
+  httpServer.listen(8080, () => {
+    console.log("Development server started on port 8080");
+  });
+}
 
 // Serve static files from the dist folder in the client directory
 app.use(express.static("../client/dist"));
 
 // For all routes, return index.html from the dist folder
 app.get("*", (req, res) => {
-  res.sendFile( "/root/sd1/client/dist/index.html");
-});
-
-httpsServer.listen(443, () => {
-  console.log("Server started on port 443");
-});
-
-// exists only to redirect http to https
-httpServer.listen(80, () => {
-  console.log("HTTP redirect server started on port 80");
+  res.sendFile("/root/sd1/client/dist/index.html");
 });
