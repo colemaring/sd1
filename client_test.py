9c45ba20a712f5cc7009c886705f3e88b7cfbeb9
Cole Maring <65455664+colemaring@users.noreply.github.com>
2025-01-14 19:13:21 -0500

    update structure (not final)

--- client_test.py
+++ client_test.py
@@ -1,11 +1,13 @@
 # This file serves as an example of how to connect to the WebSockets endpoint
-# as of 11/13/24, the data is just rendered at https://aifsd.xyz/wstest
+# as of 1/14/24, the data is just rendered at https://aifsd.xyz/wstest
 
 import asyncio
 import websockets
 import random
 import json
 from datetime import datetime
+
+timeoutSeconds = 0.5
 
 async def connect():
     uri = "wss://aifsd.xyz"
@@ -15,20 +17,15 @@ async def connect():
         while True:
             # Generate dummy data
             data = {
-                "timestamp": datetime.utcnow().isoformat() + "Z",
-                "driver": "John Doe",
-                "var1": random.choice([True, False]),
-                "var2": random.choice([True, False]),
-                "var3": random.choice([True, False]),
-                "var4": random.choice([True, False]),
-                "var5": random.choice([True, False]),
-                "var6": random.choice([True, False]),
-                "var7": random.choice([True, False]),
-                "var8": random.choice([True, False]),
-                "var9": random.choice([True, False]),
-                "var10": random.choice([True, False]),
-                "handsOnWheel": f"{random.randint(10000, 30000)}ms",
-                "handsOffWheel": f"{random.randint(100, 1000)}ms"
+                "Timestamp": datetime.utcnow().isoformat() + "Z",
+                "Driver": "John Doe",
+                "Drinking": random.choice([True, False]),
+                "Eating": random.choice([True, False]),
+                "Phone": random.choice([True, False]),
+                "SeatbeltOn": random.choice([True, False]),
+                "Sleeping": random.choice([True, False]),
+                "Smoking": random.choice([True, False]),
+                "handsOnWheel": random.choice([0, 1, 2]), # how many hands are currently on the wheel
             }
 
             # Convert the data to JSON string
@@ -36,14 +33,13 @@ async def connect():
 
             # Send the JSON data to the server
             await websocket.send(json_data)
-            print(f"Sent to server: {json_data}")
+            print("Sent message to server")
 
-            # Receive a message from the server
-            response = await websocket.recv()
-            print(f"Received from server: {response}")
+            # for debugging, not needed
+            # response = await websocket.recv()
+            # print(f"Received from server: {response}")
 
-            # Wait for 3 seconds before sending the next data
-            await asyncio.sleep(3)
+            await asyncio.sleep(timeoutSeconds)
 
 # Run the connect function
 asyncio.get_event_loop().run_until_complete(connect())
