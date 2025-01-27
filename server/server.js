a2dc058dbae048434979702bbf6dc80d96a7d632
Cole Maring <65455664+colemaring@users.noreply.github.com>
2025-01-27 15:47:26 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 const db = require("./db");
 const api = require("./api");
 
-const isDev = process.argv.includes("dev"); // test
+const isDev = process.argv.includes("dev"); 
 
 let httpsServer, httpServer;
 
